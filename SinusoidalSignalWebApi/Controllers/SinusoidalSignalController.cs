a09d700b4a67b54f5b093f085c088bdd74b68128
Лера Гордеева <valeri.gorde@mail.ru>
2023-09-12 02:11:31 +0300

    Отделена логика от контроллера

--- SinusoidalSignalWebApi/Controllers/SinusoidalSignalController.cs
+++ SinusoidalSignalWebApi/Controllers/SinusoidalSignalController.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System;
 using SinusoidalSignalWebApi.Models;
-using System.Diagnostics.CodeAnalysis;
+using SinusoidalSignalWebApi.Services;
 
 namespace SinusoidalSignalWebApi.Controllers
 {
@@ -10,62 +10,20 @@ namespace SinusoidalSignalWebApi.Controllers
     [Route("[controller]")]
     public class SinusoidalSignalController : ControllerBase
     {
-        private const int ImageWidth = 1200;
-        private const int ImageHeight = 900;
-
         [HttpPost]
         public IActionResult GenerateSignal(SinusoidalSignalParams parametrs)
         {
-            double A = parametrs.A;
-            double Fd = parametrs.Fd;
-            double Fs = parametrs.Fs;
-            int N = parametrs.N;
-
-            int totalPoints = (int)(Fd * N / Fs);
-            double Td = 1 / Fd;
-
-            Bitmap image = new Bitmap(ImageWidth, ImageHeight);
-            Graphics graphics = Graphics.FromImage(image);
-            graphics.Clear(Color.White);
-
-            // Режим сглаживания 
-            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-
-            // Массив для хранения точек
-            double[] points = new double[totalPoints];
-            for (int i = 0; i < totalPoints; i++)
+            try
             {
-                double t = i * Td;
-                points[i] = A * Math.Sin(2 * Math.PI * Fs * t);
+                MemoryStream imageStream = Calculating.GenerateSignal(parametrs);
+
+                // Отправляем изображение клиенту в виде графического файла
+                return File(imageStream, "image/png", "sinusoidal_signal.png");
             }
-
-            // Находим наибольшее значение точки
-            double maxPoint = points.Max();
-
-            // Задаём масштаб изображения
-            double scale = (ImageHeight - 1) / (2 * maxPoint);
-
-            for (int i = 0; i < totalPoints - 1; i++)
+            catch(Exception ex)
             {
-                // Вычисляем x координату первой и второй точки
-                int x1 = i * (ImageWidth - 1) / (totalPoints - 1);
-                int x2 = (i + 1) * (ImageWidth - 1) / (totalPoints - 1);
-
-                // Вычисляем y координату первой и второй точки с учётом масштаба
-                int y1Scaled = (int)((ImageHeight - 1) / 2 - (points[i] * scale));
-                int y2Scaled = (int)((ImageHeight - 1) / 2 - (points[i + 1] * scale));
-
-                // Рисуем линию между двумя точками
-                graphics.DrawLine(new Pen(Color.Black, 3f), x1, y1Scaled, x2, y2Scaled);
-            }
-
-            // Создаём поток для сохранения изображения
-            var imageStream = new MemoryStream();
-            image.Save(imageStream, System.Drawing.Imaging.ImageFormat.Png);
-            imageStream.Position = 0;
-
-            // Отправляем изображение клиенту в виде графического файла
-            return File(imageStream, "image/png", "sinusoidal_signal.png");
+                return StatusCode(500, $"Произошла ошибка при генерации изображения {ex.Message}");
+            }    
         }
     }
 }
---
+++ SinusoidalSignalWebApi/Services/Calculating.cs
@@ -0,0 +1,64 @@
+﻿using SinusoidalSignalWebApi.Models;
+using System.Drawing;
+using System;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using SkiaSharp;
+
+namespace SinusoidalSignalWebApi.Services
+{
+    public static class Calculating
+    {
+        private const int ImageWidth = 1200;
+        private const int ImageHeight = 900;
+        public static MemoryStream GenerateSignal(SinusoidalSignalParams parametrs)
+        {
+            double A = parametrs.A;
+            double Fd = parametrs.Fd;
+            double Fs = parametrs.Fs;
+            int N = parametrs.N;
+            // Количество выборок
+            int totalPoints = (int)(Fd * N / Fs);
+            double Td = 1 / Fd;
+
+            using (var bitmap = new SKBitmap(ImageWidth, ImageHeight))
+            using (var canvas = new SKCanvas(bitmap))
+            {
+                canvas.Clear(SKColors.White);
+
+                // Массив для хранения точек
+                double[] points = new double[totalPoints];
+                for (int i = 0; i < totalPoints; i++)
+                {
+                    double t = i * Td;
+                    points[i] = A * Math.Sin(2 * Math.PI * Fs * t);
+                }
+
+                // Находим наибольшее значение точки
+                double maxPoint = points.Max();
+
+                // Задаём масштаб изображения
+                double scale = (ImageHeight - 1) / (2 * maxPoint);
+
+                for (int i = 0; i < totalPoints - 1; i++)
+                {
+                    // Вычисляем x координату первой и второй точки
+                    int x1 = i * (ImageWidth - 1) / (totalPoints - 1);
+                    int x2 = (i + 1) * (ImageWidth - 1) / (totalPoints - 1);
+
+                    // Вычисляем y координату первой и второй точки с учётом масштаба
+                    int y1Scaled = (int)((ImageHeight - 1) / 2 - (points[i] * scale));
+                    int y2Scaled = (int)((ImageHeight - 1) / 2 - (points[i + 1] * scale));
+
+                    canvas.DrawLine(x1, y1Scaled, x2, y2Scaled, new SKPaint { Color = SKColors.Black });
+                }
+
+                var imageStream = new MemoryStream();
+                bitmap.Encode(imageStream, SKEncodedImageFormat.Png, 100);
+                imageStream.Position = 0;
+
+                return imageStream;
+            }
+        }
+    }
+}
